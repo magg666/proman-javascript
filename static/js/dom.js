5713d01737f359b21ff1eed20916230a63f08241
Kacper Mitkowski <kacper.mitkowski@gmail.com>
2019-03-12 18:34:53 +0100

    Create Board finished

--- static/js/dom.js
+++ static/js/dom.js
@@ -1,5 +1,5 @@
 import {dataHandler} from "./data_handler.js";
-import {createCard} from "./create_dom.js";
+import {createCard, createBoard} from "./create_dom.js";
 export {dom}
 
 // It uses data_handler.js to visualize elements
@@ -12,11 +12,7 @@ let dom = {
         // shows boards appending them to #boards div
         // it adds necessary event listeners also
 
-        let boardsDiv = document.querySelector('#boards');
-
-        for(let board of boards) {
-            createCard();
-        }
+        createBoard();
     },
     loadCards: function (boardId) {
         // retrieves cards and makes showCards called
