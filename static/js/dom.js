6849cfe8148959e98ce09488c1cf9ef62f5e39d2
magg666 <mw23127@gmail.com>
2019-03-12 22:32:49 +0100

    added function to show cards

--- static/js/dom.js
+++ static/js/dom.js
@@ -1,5 +1,7 @@
 import {dataHandler} from "./data_handler.js";
 import {createCard} from "./create_dom.js";
+import {makeTextEditableOnClick, makeTextReturnToDefaultOnMouseOut} from "./cards.js";
+
 export {dom}
 
 // It uses data_handler.js to visualize elements
@@ -20,8 +22,25 @@ let dom = {
     },
     loadCards: function (boardId) {
         // retrieves cards and makes showCards called
+        dataHandler.getCardsByBoardId(boardId, this.showCards)
     },
     showCards: function (cards) {
+
+        for(let i = 0; i< cards.length; i++){
+            let boardId = 'board' + i;
+            createCard(boardId);
+            let card = document.querySelector('.card');
+            let cardTitle = document.querySelector('.card-title');
+            let cardButtonDelete = document.querySelector('.card-delete');
+            let cardText = document.querySelector('.card-text');
+            let cardSaveButtonId = 'card-save-button' + i;
+
+            cardTitle.textContent = cards[i].title;
+
+            makeTextEditableOnClick(cardText, cardSaveButtonId);
+            makeTextReturnToDefaultOnMouseOut(cardText, cardSaveButtonId);
+
+        }
         // shows the cards of a board
         // it adds necessary event listeners also
     },
