8c406cd4699608119fd46da01313d5e0ad71852c
magg666 <mw23127@gmail.com>
2019-03-14 19:51:27 +0100

    added colors

--- static/js/boards.js
+++ static/js/boards.js
@@ -9,26 +9,26 @@ function createBoard(board) {
     boardTable.setAttribute('class', 'board-container container');
     boardTable.setAttribute('id', boardId);
 
-    let rowBoardTitle = `<div class="row">
+    let rowBoardTitle = `<div class="row row-one">
     <div class="delete-button-container col-2">
-    <button id="btn-delete${board.id}">DELETE</button></div>    
+    <button class="btn-board btn btn-outline-danger btn-lg" id="btn-delete${board.id}">DELETE</button></div>    
     <div class="archive-or-restore-board-button-container col-2">
-    <button>ARCHIVE</button></div>    
+    <button class="btn-board btn btn-outline-warning btn-lg">ARCHIVE</button></div>    
     <div class="title col-6 text-center">
     <h3>BOARD TITLE</h3></div>        
     <div class="carousel-button-container col-2">
-    <button class="board-collapse-button" id="btn${board.id}" type="button" data-toggle="collapse" aria-expanded="true" data-target="#collapse${board.id}" aria-controls="collapse${board.id}">HIDE</button></div>    
+    <button class="btn-board btn btn-info board-collapse-button" id="btn${board.id}" type="button" data-toggle="collapse" aria-expanded="true" data-target="#collapse${board.id}" aria-controls="collapse${board.id}">SHOW</button></div>    
     </div>`;
     boardTable.insertAdjacentHTML('beforeend', rowBoardTitle);
 
     let divBoardBody = `<div class="board-collapse-container collapse" id="collapse${board.id}">
     <div class="row">
     <div class="add-new-card-button-container col-6">
-    <button class="new-card-button">ADD NEW CARD</button></div>
+    <button class="btn-lg btn btn-outline-primary new-card-button">ADD NEW CARD</button></div>
     <div class="make-board-private-or-public-button-container col-6">
-    <button>SHARE/UNSHARE</button></div></div>
+    <button class="float-right btn-lg btn btn-outline-success new-card-button">SHARE/UNSHARE</button></div></div>
     
-    <div class="row">
+    <div class="row row-two">
     <div class="status col-3">New</div>
     <div class="status col-3">In progress</div>
     <div class="status col-3">Testing</div>
@@ -63,7 +63,7 @@ function addListenersToElementsTable(board) {
     collapseButton.addEventListener('click', function () {
         let collapseContent = document.getElementById("collapse" + board.id);
         setTimeout(function () {
-            collapseButton.classList.contains('collapsed') ? collapseButton.textContent = 'SHOW' : collapseButton.textContent = 'HIDE'
+            collapseButton.classList.contains('collapsed') ? collapseButton.textContent = 'SHOW' : collapseButton.textContent = 'HIDE';
         }, 200)
     });
 
