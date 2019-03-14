5eba9155290de43d17b587d66a03162aeab3ccd1
magg666 <mw23127@gmail.com>
2019-03-14 08:48:15 +0100

    adding card functions

--- static/js/cards.js
+++ static/js/cards.js
@@ -1,21 +1,120 @@
-export {makeTextEditableOnClick, makeTextReturnToDefaultOnMouseOut, createCard}
+import {dom} from "./dom.js";
+import {dataHandler} from "./data_handler.js";
 
-function makeSaveButton(element, saveButtonId){
-    let saveButton = document.createElement('button');
-    saveButton.classList.add('save-button', 'btn', 'btn-info');
-    saveButton.setAttribute('id', saveButtonId);
-    saveButton.insertAdjacentHTML('afterbegin', '<i class="far fa-save"></i>');
-    element.insertAdjacentElement('afterend', saveButton);
-    saveButton.addEventListener('click', function () {
-        element.contentEditable = 'false';
-        saveButton.remove()
+export {createCard, fillCard, makeTextReturnToDefaultOnMouseOut, makeTextEditableOnClick, changeLabelOfCollapseButton, setDataAttributes}
+
+function createCard(boardProperColumn, card_data) {
+
+    let cardTemplate = `
+<div class="card">
+    <div class="card-body">
+        <div class="card-subtitle">
+            <button class="card-delete-button btn btn-sm btn-danger" title="DELETE"><i class="far fa-trash-alt"></i></button>
+            <button class="card-archive-button btn btn-sm btn-warning" title="ARCHIVE"><i class="far fa-file-archive"></i></button>
+        </div>    
+        <h4 class="card-text"></h4>        
+    </div>
+</div>
+`;
+    boardProperColumn.insertAdjacentHTML('afterbegin',cardTemplate);
+    fillCard(boardProperColumn.firstElementChild, card_data)
+}
+
+
+function fillCard(card, card_data) {
+
+    let cardId = 'card' + card_data.id;
+    card.setAttribute('id', cardId);
+    card.setAttribute('draggable', 'true');
+    drag(card);
+
+    let cardText = card.querySelector('.card-text');
+    cardText.textContent = card_data.title;
+    editableOnClick(cardText, card_data);
+
+    let cardDeleteButton = card.querySelector('.card-delete-button');
+    deleteDataOnClick(cardDeleteButton, cardId, "cards");
+
+    let cardArchiveButton = card.querySelector('card-archive-button');
+
+
+
+    setDataAttributes(card, card_data);
+    setDataAttributes(cardText, card_data);
+    setDataAttributes(cardDeleteButton, card_data);
+}
+
+function setDataAttributes(item, card_data){
+    const myDataAttributes = [
+        {attr: "data-card-id", value: card_data.id},
+        {attr: "data-board-id", value: card_data.board_id},
+        {attr: "data-status-id", value: card_data.status_id},
+        {attr: "data-order", value: card_data.order},
+    ];
+    for(let {attr, value} of myDataAttributes){
+        item.setAttribute(attr, value)
+    }
+}
+
+// drag function
+function drag(element){
+    element.addEventListener('dragstart', function (ev) {
+        ev.dataTransfer.setData("text/plain", ev.target.id);
+    });
+}
+
+// text rename handler
+function editableOnClick(element, data) {
+    element.addEventListener('dblclick', function () {
+        if(!element.isContentEditable){
+            element.contentEditable = 'true';
+            createButton(element, data)
+        }
     })
 }
+
+function createButton(element, data){
+    let saveButton = document.createElement('button');
+    saveButton.classList.add('save-button','float-right', 'btn', 'btn-sm', 'btn-success');
+    saveButton.setAttribute('title', 'SAVE TEXT or ESC TO CANCEL');
+    saveButton.insertAdjacentHTML('afterbegin', '<i class="far fa-save"></i>');
+
+    saveButton.addEventListener('click', function () {
+        element.contentEditable = 'false';
+        saveButton.remove();
+        data.title = element.textContent;
+        dataHandler.synchronise()
+    });
+
+    element.parentElement.parentElement.appendChild(saveButton);
+}
+//------------------------
+
+// delete handler
+
+function deleteDataOnClick(element, dataId, data) {
+    element.addEventListener('click', function () {
+        let confirmation = confirm("Do you really want delete card?\nIt's irreversible...");
+        confirmation === true ? deleteDataAndSave(dataId, data): null;
+    })
+}
+
+function deleteDataAndSave(dataId, data) {
+    let dataToDelete = document.getElementById(dataId);
+    let id = parseInt(dataToDelete.dataset.cardId, 10);
+    dataToDelete.remove();
+    dataHandler.deleteData(data, id);
+    dataHandler.synchronise()
+}
+
+//----------
+
+
 
 function makeTextEditableOnClick(element, saveButtonId) {
     element.addEventListener('click', function () {
         element.contentEditable = 'true';
-        if(element.nextSibling === null){
+        if (element.nextSibling === null) {
             makeSaveButton(element, saveButtonId)
         }
     })
@@ -23,7 +122,7 @@ function makeTextEditableOnClick(element, saveButtonId) {
 
 function makeTextReturnToDefaultOnMouseOut(element, saveButtonId) {
     element.addEventListener('mouseout', function () {
-        if(element.isContentEditable){
+        if (element.isContentEditable) {
             let button = document.getElementById(saveButtonId);
             element.contentEditable = 'false';
             button.remove()
@@ -31,47 +130,9 @@ function makeTextReturnToDefaultOnMouseOut(element, saveButtonId) {
     })
 }
 
-function createCard(boardId) {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.setAttribute('draggable', 'true');
-    // card.setAttribute('ondragstart', 'drag(event)');
-    card.addEventListener('dragstart', function () {
-        drag(event)
-    });
-    card.setAttribute('id', 'card1');
 
-    const cardBody = document.createElement('div');
-
-
-
-    cardBody.classList.add('card-body');
-
-    const cardTitle = document.createElement('h4');
-    cardTitle.classList.add('card-title');
-
-    const cardSubtitle = document.createElement('div');
-    cardSubtitle.classList.add('card-subtitle');
-
-    const cardButtonDelete = document.createElement('button');
-    cardButtonDelete.classList.add('card-delete');
-    cardButtonDelete.setAttribute('title', 'Delete Card');
-    cardButtonDelete.insertAdjacentHTML('afterbegin', '<i class="far fa-trash-alt"></i>');
-
-    const cardText = document.createElement('textarea');
-    cardText.classList.add('card-text');
-
-    cardSubtitle.appendChild(cardButtonDelete);
-
-    cardBody.appendChild(cardTitle);
-    cardBody.appendChild(cardSubtitle);
-    cardBody.appendChild(cardText);
-
-    card.appendChild(cardBody);
-    const board = document.getElementById(boardId);
-    let cardContainer = board.querySelector('.card-container1');
-    cardContainer.appendChild(card);
-}
-function drag(ev) {
-    ev.dataTransfer.setData("text/plain", ev.target.id);
+function changeLabelOfCollapseButton(collapseButton) {
+    setTimeout(function () {
+        collapseButton.classList.contains('collapsed') ? collapseButton.textContent = 'SHOW' : collapseButton.textContent = 'HIDE'
+    }, 200)
 }
