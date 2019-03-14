f3039da87f620df62fcbd0756b988c7f80fd965c
Michal Kalinowski <subaru.kali@gmail.com>
2019-03-14 12:15:50 +0100

    allowDrop & drop added

--- static/js/event_handlers.js
+++ static/js/event_handlers.js
@@ -1,59 +1,73 @@
-export {drag, editableOnClick, deleteDataOnClick}
+export {drag, editableOnClick, deleteDataOnClick, drop, allowDrop}
 import {dataHandler} from "./data_handler.js";
 
 
 // drag function
-function drag(element){
-    element.addEventListener('dragstart', function (ev) {
-        ev.dataTransfer.setData("text/plain", ev.target.id);
-    });
+function drag(element) {
+	element.addEventListener('dragstart', function (ev) {
+		ev.dataTransfer.setData("text/plain", ev.target.id);
+	});
 }
 
 // text rename handler
 function editableOnClick(element, data) {
-    element.addEventListener('dblclick', function () {
-        if(!element.isContentEditable){
-            element.contentEditable = 'true';
-            updateText(element, data)
-        }
-    })
+	element.addEventListener('dblclick', function () {
+		if (!element.isContentEditable) {
+			element.contentEditable = 'true';
+			updateText(element, data)
+		}
+	})
 }
 
-function updateText(element, data){
-    let saveButton = document.createElement('button');
-    saveButton.classList.add('save-button','float-right', 'btn', 'btn-sm', 'btn-success');
-    saveButton.setAttribute('title', 'SAVE TEXT or ESC TO CANCEL');
-    saveButton.insertAdjacentHTML('afterbegin', '<i class="far fa-save"></i>');
+function updateText(element, data) {
+	let saveButton = document.createElement('button');
+	saveButton.classList.add('save-button', 'float-right', 'btn', 'btn-sm', 'btn-success');
+	saveButton.setAttribute('title', 'SAVE TEXT or ESC TO CANCEL');
+	saveButton.insertAdjacentHTML('afterbegin', '<i class="far fa-save"></i>');
 
-    saveButton.addEventListener('click', function () {
-        element.contentEditable = 'false';
-        saveButton.remove();
-        data.title = element.textContent;
-        dataHandler.synchronise()
-    });
+	saveButton.addEventListener('click', function () {
+		element.contentEditable = 'false';
+		saveButton.remove();
+		data.title = element.textContent;
+		dataHandler.synchronise()
+	});
 
-    element.parentElement.parentElement.appendChild(saveButton);
+	element.parentElement.parentElement.appendChild(saveButton);
 }
+
 //------------------------
 
 // delete handler
 
 function deleteDataOnClick(element, dataId, data) {
-    element.addEventListener('click', function () {
-        let confirmation = confirm("Do you really want delete card?\nIt's irreversible...");
-        confirmation === true ? deleteDataAndSave(dataId, data): null;
-    })
+	element.addEventListener('click', function () {
+		let confirmation = confirm("Do you really want delete card?\nIt's irreversible...");
+		confirmation === true ? deleteDataAndSave(dataId, data) : null;
+	})
 }
 
 function deleteDataAndSave(dataId, data) {
-    let dataToDelete = document.getElementById(dataId);
-    let id = parseInt(dataToDelete.dataset.cardId, 10);
-    dataToDelete.remove();
-    dataHandler.deleteData(data, id);
-    dataHandler.synchronise()
+	let dataToDelete = document.getElementById(dataId);
+	let id = parseInt(dataToDelete.dataset.cardId, 10);
+	dataToDelete.remove();
+	dataHandler.deleteData(data, id);
+	dataHandler.synchronise()
 }
 
 
+function allowDrop(element) {
+	element.addEventListener('dragover', function (ev) {
+		ev.preventDefault();
+	})
+}
+
+function drop(element) {
+	element.addEventListener('drop', function (ev) {
+		ev.preventDefault();
+		let data = ev.dataTransfer.getData('text/plain');
+		ev.target.appendChild(document.getElementById(data))
+	})
+}
 
 // make one card/board
 // for one card/board add event handlery
@@ -72,9 +86,6 @@ function deleteDataAndSave(dataId, data) {
 // CHANGE TEXT
 // ARCHIVE
 // CHANGE STATUS
-
-
-
 
 
 // let deleteButton;
