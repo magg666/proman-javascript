e39823135b4a7d83ed45d9a02ee473c1ec038b66
magg666 <mw23127@gmail.com>
2019-03-15 12:19:00 +0100

    added both functions main

--- static/js/main.js
+++ static/js/main.js
@@ -4,22 +4,34 @@ import {addListenerButtonModal} from "./modal_login.js";
 import {main} from "./test.js";
 
 // This function is to initialize the application
-function init() {
-    main();
-
-    // init data
-    // dataHandler.init();
-    setTimeout(function (){
-        dataHandler.init()
-    }, 40000);
-    setTimeout(function (){
-        dom.loadBoards()
-    }, 40000)
-
+// function init() {
+//     main();
+//
+//     // init data
+//     // dataHandler.init();
+//     setTimeout(function () {
+//         dataHandler.init()
+//     }, 40000);
+//     setTimeout(function () {
+//         dom.loadBoards()
+//     }, 40000)
+//
+// }
     // loads the boards to the screen
-    ;
-    // addListenerButtonModal("register-button", "registration");
-    // addListenerButtonModal("login-button", "login");
+
+    //
+
+
+
+// This function is to initialize the application
+function init() {
+    // init data
+    dataHandler.init();
+    // loads the boards to the screen
+    dom.loadBoards();
+    addListenerButtonModal("register-button", "registration");
+    addListenerButtonModal("login-button", "login");
 }
+
 
 init();
